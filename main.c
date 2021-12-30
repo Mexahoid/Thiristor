cc6513a8aff6a6e34ddda3a2ca0d21755682d012
Max <mexahoidbloodwing@Gmail.com>
2021-12-30 18:51:10 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,30 +1,74 @@
 #include "mbed.h"
  
 DigitalOut mainpin(PC_6);
-DigitalIn pinx(PC_8, PullDown);
+DigitalIn syncpin(PC_8, PullDown);
+
+
+const int minPerc = 0;
+const int maxPerc = 10000;
+
+int percentage = 1700; // 17%
+
+int prevPercentage = 0;
+
+int percRed = percentage;
+int minRed = minPerc;
+int maxRed = maxPerc;
+
+void reduce()
+{
+    if (maxPerc % percentage == 0)
+    {
+        maxRed /= percentage;
+        minRed /= percentage;
+        percRed = 1;
+        return;
+    }
+    
+    for (int i = minPerc; i < maxRed; i++)
+    {
+        if (i < 2)
+            continue;
+        
+        if (minRed % i == 0 && percRed % i == 0 && maxRed == 0)
+        {
+            minRed /= i;
+            percRed /= i;
+            maxRed /= i;
+        }
+    }
+    
+    prevPercentage = percentage;
+}
+
 
 int main() {
-    int count = 0; 
+    int current = 0; 
     int present = 0;
+    int state = 1;
     while(1) {
-        int value = pinx.read();
+        if (prevPercentage != percentage)
+            reduce();
+        int value = syncpin.read();
         
         if (value == 1 && present == 0)
         {
-            count++;
+            current++;
+            if (current <= percRed)
+            {
+                mainpin = state;
+                state = !state;
+                mainpin = state;
+                state = !state;
+            }
+            if (current >= maxRed)
+                current = minRed;
             present = 1;
         }
         
         if (value == 0 && present == 1)
         {
             present = 0;
-        }
-        
-        if (count > 1)
-        {
-            mainpin = 1;
-            mainpin = 0;
-            count = 0;
         }
     }
 }
