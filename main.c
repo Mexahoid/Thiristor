8075f7abeb7374c0c1c36de884b33300ed1fe91b
Max <mexahoidbloodwing@Gmail.com>
2022-01-16 14:09:42 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,75 +4,107 @@ DigitalOut mainpin(PC_6);
 DigitalIn syncpin(PC_8, PullDown);
 
 
-const int minPerc = 0;
-const int maxPerc = 10000;
+int p = 170; // 17%
+int N = 10;
 
-int percentage = 1700; // 17%
-
-int prevPercentage = 0;
-
-int percRed = percentage;
-int minRed = minPerc;
-int maxRed = maxPerc;
-
-void reduce()
+void send_pulse()
 {
-    percRed = percentage;
-    minRed = minPerc;
-    maxRed = maxPerc;
-    
-    if (maxPerc % percentage == 0)
-    {
-        maxRed /= percentage;
-        minRed /= percentage;
-        percRed = 1;
-        return;
-    }
-    
-    for (int i = minPerc; i < maxRed; i++)
-    {
-        if (i < 2)
-            continue;
-        
-        if (minRed % i == 0 && percRed % i == 0 && maxRed == 0)
-        {
-            minRed /= i;
-            percRed /= i;
-            maxRed /= i;
-        }
-    }
-    
-    prevPercentage = percentage;
+    mainpin = 1;
+    mainpin = 0;
 }
 
+int is_it_time(int last, int current, int count, int max)
+{
+    if (current == 1)
+        return 1;
+    
+    int ratio = max / count;
+    
+    if (current == last + ratio)
+        return 1;
+    
+    return 0;
+}
 
-int main() {
-    int current = 0; 
+int main()
+{
+    int lambda = 0;
+    
+    int p = 170; // 170/1000
+    int N = 10;
+    
+    int A = p * N / 1000;  // Целая часть
+    int B = p * N % 1000;  // Остаток
+
+    lambda = 0;
+    
+    int period = 0;
+    
+    
     int present = 0;
-    int state = 1;
-    while(1) {
-        if (prevPercentage != percentage)
-            reduce();
+    
+    int lA = (lambda + B) / 1000;
+    int last = 1;
+    
+    int next = 0;
+    
+    
+    while (1)
+    {
         int value = syncpin.read();
         
         if (value == 1 && present == 0)
         {
-            current++;
-            if (current <= percRed)
-            {
-                mainpin = state;
-                state = !state;
-                mainpin = state;
-                state = !state;
-            }
-            if (current >= maxRed)
-                current = minRed;
+            next = !next;
+                
+            
             present = 1;
+            
+            if (next)
+            {
+                if (period == 0)
+                {
+                    lA = (lambda + B) / 1000;
+                    last = 1;
+                }
+                period++;
+                present = 1;
+            }
+            
+            
+            if (lA == 0)
+            {
+                if (is_it_time(last, period, 1, N))
+                {
+                    send_pulse();
+                }
+            }
+            else
+            {
+                if (is_it_time(last, period, 2 * lA, N))
+                {
+                    send_pulse();
+                    if (!next)
+                        last = period;
+                 }
+            }
+            
+            
+            if (period == N && next)
+            {
+                lambda = (lambda + B) % 1000;
+                period = 0;
+            }
+            
+            
         }
         
         if (value == 0 && present == 1)
         {
             present = 0;
         }
+        
     }
+    
+    return 0;
 }
